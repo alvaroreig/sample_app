610e18947728c1f9991431cb3962a60cb23ef00c
alvaroreig <argregma@gmail.com>
2013-09-27 17:06:47 +0200

    after exercise 5.6.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,7 @@ describe "Static pages" do
   describe "Contact" do
     before { visit contact_path }
 
-    it { should have_content('Contact Us') }
-    it { should have_title(full_title('Contact Us')) }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
