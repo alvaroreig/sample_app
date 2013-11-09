5de0ac0a4b599d30dc6e55752568f638814ed4d5
alvaroreig <argregma@gmail.com>
2013-11-09 12:56:15 +0100

    after linkify

--- android_course/workspace/Infocomer/src/com/galileo/cursoandroid/StoreActivity.java
+++ android_course/workspace/Infocomer/src/com/galileo/cursoandroid/StoreActivity.java
@@ -1,14 +1,19 @@
 package com.galileo.cursoandroid;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,8 +22,11 @@ public class StoreActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		/*Set custom titlebar and layout*/
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.activity_store);
-
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
+		
 		/* Get intent and recover the store values */
 		Intent intent = getIntent();
 		String storeName = intent.getStringExtra(MainActivity.STORE_NAME);
@@ -27,16 +35,35 @@ public class StoreActivity extends Activity {
 				.getStringExtra(MainActivity.STORE_TELEPHONE);
 		String storeTimeOpen = intent
 				.getStringExtra(MainActivity.STORE_TIME_OPEN);
+		String storeEmail = intent
+				.getStringExtra(MainActivity.STORE_EMAIL);
+		String storeWebsite = intent
+				.getStringExtra(MainActivity.STORE_WEBSITE);
 
 		/* Populate the elements with the recovered values */
 		TextView textView = (TextView) findViewById(R.id.txtViewName);
 		textView.setText(storeName);
+		Linkify.addLinks(textView, Linkify.ALL);
+		
 		textView = (TextView) findViewById(R.id.txtViewAddress);
 		textView.setText(storeAddress);
+		Linkify.addLinks(textView, Linkify.ALL);
+		
 		textView = (TextView) findViewById(R.id.txtViewTelephone);
 		textView.setText(storeTelephone);
+		Linkify.addLinks(textView, Linkify.ALL);
+		
+		textView = (TextView) findViewById(R.id.txtViewEmail);
+		textView.setText(storeEmail);
+		Linkify.addLinks(textView, Linkify.ALL);
+		
+		textView = (TextView) findViewById(R.id.txtViewWebsite);
+		textView.setText(storeWebsite);
+		Linkify.addLinks(textView, Linkify.ALL);
+		
 		textView = (TextView) findViewById(R.id.txtViewTImesOpenData);
 		textView.setText(storeTimeOpen);
+		Linkify.addLinks(textView, Linkify.ALL);
 
 		/* Set up call button */
 		Button callButton = (Button) findViewById(R.id.btnCall);
