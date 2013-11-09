714a0dcfe4b7a4cce3543123bfcd422ee281af54
alvaroreig <argregma@gmail.com>
2013-11-09 16:38:11 +0100

    after refactoring

--- android_course/workspace/Infocomer/src/com/galileo/cursoandroid/PhotographyDetailActivity.java
+++ android_course/workspace/Infocomer/src/com/galileo/cursoandroid/PhotographyDetailActivity.java
@@ -12,27 +12,31 @@ public class PhotographyDetailActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		/*Set up custom title bar*/
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.activity_photography_detail);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
+		
+		/*Load data from the intent*/
 		String picture_name = getIntent().getStringExtra(MainActivity.STORE_PICTURE);
 		String comments = getIntent().getStringExtra(MainActivity.STORE_COMMENTS);
 		
-		ImageView image = (ImageView) findViewById(R.id.imageView1);
+		/*Populate data into the elements*/
+		ImageView image = (ImageView) findViewById(R.id.imgViewMain);
 		image.setImageResource(getDrawableResourceByName(picture_name));
 		
 		TextView commentsTextView = (TextView) findViewById(R.id.txtViewComments);
 		commentsTextView.setText(comments);
 	}
 
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		//getMenuInflater().inflate(R.menu.photography_detail, menu);
 		return true;
 	}
 	
-	/* Method to recover Strings resource using a String instead of an int */
+	/* Method to recover drawable resources using a String instead of an int */
 	private int getDrawableResourceByName(String aString) {
 		String packageName = getPackageName();
 		int resId = getResources()
