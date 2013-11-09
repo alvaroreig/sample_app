2e3143cc30b56e2b1b69f28df6a241156637cf05
alvaroreig <argregma@gmail.com>
2013-11-09 12:23:16 +0100

    added custom title bar in main activity

--- android_course/workspace/Infocomer/src/com/galileo/cursoandroid/MainActivity.java
+++ android_course/workspace/Infocomer/src/com/galileo/cursoandroid/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
@@ -39,7 +40,9 @@ public class MainActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.activity_main);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
 
 		/*Link the ViewList with the stores ArrayList*/
 		SimpleAdapter adapter = new SimpleAdapter(this, stores,
