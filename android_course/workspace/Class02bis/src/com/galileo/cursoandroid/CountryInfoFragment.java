209af5757529b8beafc5e494093662d43df60ada
alvaroreig <argregma@gmail.com>
2013-11-12 18:01:31 +0100

    after trouble

--- android_course/workspace/Class02bis/src/com/galileo/cursoandroid/CountryInfoFragment.java
+++ android_course/workspace/Class02bis/src/com/galileo/cursoandroid/CountryInfoFragment.java
@@ -1,17 +1,17 @@
 package com.galileo.cursoandroid;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 
-public class CountryInfoFragment extends Fragment {
+public class CountryInfoFragment extends FragmentActivity {
 	private WebView webView;
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
-
+		String country = ((CountryDetailActivity)getActivity()).getCountry();
 		super.onActivityCreated(savedInstanceState);
 	}
 
