128aadcd8b0c67b81e8886cb25f487e849dde41c
alvaroreig <argregma@gmail.com>
2013-12-29 13:44:09 +0100

    after hw53

--- mongodb_m101p_course/hw5/hw53.js
+++ mongodb_m101p_course/hw5/hw53.js
@@ -5,8 +5,6 @@ db.hw53.aggregate([
 	,{$match:{$or : [{"scores.type" : "homework"},{"scores.type" : "exam"}]}}
 	,{$group:{_id:{"student_id":"$student_id","class_id":"$class_id"},"avg_scores":{"$avg":"$scores.score"}}}
 	,{$group:{_id:{"class_id":"$_id.class_id"},"avg_scores":{"$avg":"$avg_scores"}}}
-	// ,{$group:{_id:{"_id"}}
-	// ,{$match:{$or : [{"_id.state" : "CA"},{"_id.state" : "NY"}]}}
-	// ,{$match:{ "population":{$gt:25000}}}
-	// ,{ $group:{ _id : "null", avgPop : { $avg : "$population" } } }
+	,{$sort: {"avg_scores":-1}}
+	,{$limit:1}
 ])
