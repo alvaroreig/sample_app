5a8d1f7eefa89ff78dac8da1b25f2a29ad98a1a6
alvaroreig <argregma@gmail.com>
2013-12-29 14:41:53 +0100

    after hw54

---
+++ mongodb_m101p_course/hw5/hw54.js
@@ -0,0 +1,7 @@
+use test;
+
+db.zips.aggregate([
+ {$project: {first_char: {$substr : ["$city",0,1]},pop:1,city:1}}
+ ,{$match:{"first_char":{$regex:'[0-9]'}}}
+ ,{$group:{_id:null,"total_population":{"$sum":"$pop"}}}
+])
