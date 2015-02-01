2cb92a784b27dab4e01106bc4ce8160a097871f5
alvaroreig <alvaroreig@gmail.com>
2015-02-01 10:27:21 +0100

    after computing cost for multiple variables

--- machine_learning_stanford/mlclass-ex1-008/mlclass-ex1/computeCostMulti.m
+++ machine_learning_stanford/mlclass-ex1-008/mlclass-ex1/computeCostMulti.m
@@ -14,6 +14,12 @@ J = 0;
 %               You should set J to the cost.
 
 
+for i =1:m
+	% non-vector implementation
+	% sumatorio = sumatorio + ((theta(1,1) + theta(2,1)*X(i,2) - y(i,1))^2);
+	% vector implementation
+	J = J + (((theta'*X(i,:)'- y(i))^2)/(2*m));
+endfor
 
 
 
