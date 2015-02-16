5f429ff336b9d27eb04a945144be7dd5c4465e78
alvaroreig <alvaroreig@gmail.com>
2015-02-16 18:54:19 +0100

    fixing mistake

--- machine_learning_stanford/mlclass-ex2-008/mlclass-ex2/costFunction.m
+++ machine_learning_stanford/mlclass-ex2-008/mlclass-ex2/costFunction.m
@@ -21,17 +21,20 @@ grad = zeros(size(theta));
 %
 
 % non vector
-
 sumatorio = 0;
 
-% inefficient implementation, must vectorize it when I have the time
+for i=1:m
+	sumatorio = sumatorio - y(i)*log(sigmoid(theta'*X(i,:)')) - (1-y(i))*log(1-sigmoid(theta'*X(i,:)'));
+endfor
+
+
+% inefficent implementation, must vectorize it when I have the time
+
 for j=1:size(theta)
 	for i=1:m
-		sumatorio = sumatorio - y(i)*log(sigmoid(theta'*X(i,:)')) - (1-y(i))*log(1-sigmoid(theta'*X(i,:)'));
 		grad(j) = grad(j) + [sigmoid(theta'*X(i,:)') - y(i)]*X(i,j);
 	endfor
 endfor
-
 
 J = sumatorio/m;
 grad = grad ./m;
