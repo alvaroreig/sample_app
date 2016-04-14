a03bfda5cbfc195dfce9a898460ee81c2218d463
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2016-04-14 16:35:41 +0000

    connected to mongo using mongoose

---
+++ mean/app_server/models/db.js
@@ -0,0 +1,42 @@
+var mongoose = require( 'mongoose' );
+
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+// Intercept mongoose events and log them
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+// We need to call this function before stopping
+var gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+// Listeners for nodemon, nodejs and heroku
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
