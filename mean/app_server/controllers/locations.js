d3005add2f03b5bf23350db44561dee349075719
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2016-04-25 15:16:53 +0000

    new comments using the web interface call POST API

--- mean/app_server/controllers/locations.js
+++ mean/app_server/controllers/locations.js
@@ -82,8 +82,7 @@ var renderDetailPage = function (req, res, locDetail) {
     });
 };
 
-/* GET 'Location info' page */
-module.exports.locationInfo = function(req, res){
+var getLocationInfo = function (req, res, callback){
     var requestOptions, path;
     path = "/api/locations/" + req.params.locationid;
     requestOptions = {
@@ -100,7 +99,7 @@ module.exports.locationInfo = function(req, res){
                     lng : body.coords[0],
                     lat : body.coords[1]
                 };
-                renderDetailPage(req, res, data);
+                callback(req, res, data);
             }else{
                 _showError(req, res, response.statusCode);
             }
@@ -122,14 +121,51 @@ module.exports.locationInfo = function(req, res){
             content : content
         });
     };
+}
+
+/* GET 'Location info' page */
+module.exports.locationInfo = function(req, res){
+    getLocationInfo(req, res, function(req, res, responseData){
+        renderDetailPage(req, res, responseData);
+    })
 };
+
+var renderReviewForm = function (req, res, locDetail) {
+    res.render('location-review-form', {
+        title: 'Review ' + locDetail.name + ' on Loc8r',
+        pageHeader: { title: 'Review ' + locDetail.name }
+});
+}; 
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', {
-        title: 'Review Starcups on Loc8r',
-        pageHeader: {
-            title: 'Review Starcups'
-        }
+    getLocationInfo(req, res, function(req, res, responseData) {
+        renderReviewForm(req, res, responseData);
     });
+};
+
+// POST locations/:locationid/reviews/new
+module.exports.doAddReview = function(req, res){
+    var requestOptions, path, locationid, postdata;
+    locationid = req.params.locationid;
+    path = "/api/locations/" + locationid + '/reviews';
+    postdata = {
+        author: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        reviewText: req.body.review
+    };
+    requestOptions = {
+        url : apiOptions.server + path,
+        method : "POST",
+        json : postdata
+    };
+    request(
+        requestOptions,
+        function(err, response, body) {
+            if (response.statusCode === 201) {
+                res.redirect('/location/' + locationid);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
+        });
 };
