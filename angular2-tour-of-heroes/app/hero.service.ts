4eec20be3c7ee390863696ebbc03eddc5d325cb0
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2016-05-02 12:20:35 +0200

    navigation finished

--- angular2-tour-of-heroes/app/hero.service.ts
+++ angular2-tour-of-heroes/app/hero.service.ts
@@ -5,5 +5,10 @@ import {HEROES} from './mock-heroes';
 export class HeroService {
 	getHeroes() {
 		return Promise.resolve(HEROES);
-  }
+  },
+  getHero(id: number) {
+  	return Promise.resolve(HEROES).then(
+    	heroes => heroes.filter(hero => hero.id === id)[0]
+  	);
+	}
 }
