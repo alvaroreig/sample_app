caa133eaa7c53ede6f1d091491e009103db992a4
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2016-06-25 14:00:23 +0200

    after bootstrap,material design,end of chapter2

--- laravel/learninglaravel/resources/views/master.blade.php
+++ laravel/learninglaravel/resources/views/master.blade.php
@@ -1,11 +1,17 @@
 <html>
 <head>
     <title> @yield('title') </title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+  <!-- Material Design fonts -->
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
+
+  <!-- Bootstrap Material Design -->
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap-material-design.css">
+  <link rel="stylesheet" type="text/css" href="/css/ripples.min.css">
+
 </head>
 <body>
 
@@ -13,5 +19,17 @@
 
 @yield('content')
 
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // This command is used to initialize some elements and make them work properly
+        $.material.init();
+    });
+</script>
 </body>
+
 </html>
