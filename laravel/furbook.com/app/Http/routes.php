2ccd36343a6a6ce8c3b02bc03ed8e42556bac788
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2016-06-25 11:50:38 +0200

    first furbook stub

---
+++ laravel/furbook.com/app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function() {
+	return redirect('cats');
+});
+
+Route::get('cats', function() {
+	return 'All cats';
+});
+
+Route::get('cats/{id}', function($id) {
+	sprintf('Cat #%d', $id);
+})->where('id', '[0-9]+');
+
+Route::get('about', function() {
+	return view('about')->with('number_of_cats', 9000);
+});
