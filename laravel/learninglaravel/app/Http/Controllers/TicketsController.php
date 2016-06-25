dfa8c0a09d2a4e80c173ec5f6b4fc52a73e812bb
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2016-06-25 16:10:56 +0200

    after GET /tickets and GET /ticket/slug

--- laravel/learninglaravel/app/Http/Controllers/TicketsController.php
+++ laravel/learninglaravel/app/Http/Controllers/TicketsController.php
@@ -18,14 +18,28 @@ class TicketsController extends Controller
 	public function store(TicketFormRequest $request)
 	{
 		$slug = uniqid();
-    $ticket = new Ticket(array(
-        'title' => $request->get('title'),
-        'content' => $request->get('content'),
-        'slug' => $slug
-    ));
+		$ticket = new Ticket(array(
+			'title' => $request->get('title'),
+			'content' => $request->get('content'),
+			'slug' => $slug
+			));
 
-    $ticket->save();
+		$ticket->save();
 
-    return redirect('/contact')->with('status', 'Your ticket has been created! Its unique id is: '.$slug);
+		return redirect('/contact')->with('status', 'Your ticket has been created! Its unique id is: '.$slug);
+	}
+
+	//GET /tickets
+	public function index()
+	{
+		$tickets = Ticket::all();
+		return view('tickets.index', compact('tickets'));
+	}
+
+	//GET /tickets/{slug}
+	public function show($slug)
+	{
+		$ticket = Ticket::whereSlug($slug)->firstOrFail();
+		return view('tickets.show', compact('ticket'));
 	}
 }
